8f3f00ce3aa428fb5385e051e82d7c57809bfc4a
Wojciech Malarski <w.malarski@gmail.com>
2024-11-07 21:38:16 +0100

    adds generated package and remove old components

---
+++ packages/ui/panda.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "@pandacss/dev";
+import { sp98Preset } from "@sp98/preset";
+
+export default defineConfig({
+	presets: [sp98Preset],
+	preflight: true,
+	outExtension: "mjs",
+	include: ["./src/**/*.{js,jsx,ts,tsx}"],
+	exclude: [],
+	theme: { extend: {} },
+	outdir: "../generated",
+	importMap: {
+		css: "@sp98/ui/css",
+		recipes: "@sp98/ui/recipes",
+		patterns: "@sp98/ui/patterns",
+		jsx: "@sp98/ui/jsx",
+	},
+	jsxFramework: "solid",
+});
