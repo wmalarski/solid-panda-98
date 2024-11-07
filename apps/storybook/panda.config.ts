4fdc3093f40cf58f753c372f99af751b6ea89be1
Wojciech Malarski <w.malarski@gmail.com>
2024-11-07 20:15:10 +0100

    adds some config to apps

---
+++ apps/storybook/panda.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "@pandacss/dev";
+import { sp98Preset } from "@sp98/preset";
+
+export default defineConfig({
+	presets: [sp98Preset],
+	// Whether to use css reset
+	preflight: true,
+
+	// Where to look for your css declarations
+	include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+
+	// Files to exclude
+	exclude: [],
+
+	// Useful for theme customization
+	theme: {
+		extend: {},
+	},
+
+	// The output directory for your css system
+	outdir: "styled-system",
+});
