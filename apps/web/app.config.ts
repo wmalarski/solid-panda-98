fb31ae1f8833906fd4294560c3f312cc76452732
Wojciech Malarski <w.malarski@gmail.com>
2024-11-18 20:17:45 +0100

    packages bumped

--- apps/web/app.config.ts
+++ apps/web/app.config.ts
@@ -4,13 +4,6 @@ import { visualizer } from "rollup-plugin-visualizer";
 export default defineConfig({
 	ssr: false,
 	vite: {
-		plugins: [
-			visualizer({
-				open: true,
-				gzipSize: true,
-				brotliSize: true,
-				template: "treemap",
-			}),
-		],
+		plugins: [visualizer({ open: true })],
 	},
 });
